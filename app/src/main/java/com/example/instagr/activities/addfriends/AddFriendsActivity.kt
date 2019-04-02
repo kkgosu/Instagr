429413643821d1ddc00ab116205aaba39f403b1e
bitchigosu <kostya96.08@mail.ru>
2019-04-02 14:32:59 +0300

    CommonViewModel, errorMessage handling in BaseActivity. Encapsulated navigation in InstagramBottomNavigation

--- app/src/main/java/com/example/instagr/activities/addfriends/AddFriendsActivity.kt
+++ app/src/main/java/com/example/instagr/activities/addfriends/AddFriendsActivity.kt
@@ -6,12 +6,13 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.example.instagr.R
+import com.example.instagr.activities.BaseActivity
 import com.example.instagr.activities.ViewModelFactory
 import com.example.instagr.activities.showToast
 import com.example.instagr.models.User
 import kotlinx.android.synthetic.main.activity_add_friends.*
 
-class AddFriendsActivity : AppCompatActivity(),
+class AddFriendsActivity : BaseActivity(),
     FriendsAdapter.Listener {
 
     private lateinit var mUser: User
@@ -24,8 +25,7 @@ class AddFriendsActivity : AppCompatActivity(),
         setContentView(R.layout.activity_add_friends)
 
         mAdapter = FriendsAdapter(this)
-        mViewmodel = ViewModelProviders.of(this, ViewModelFactory())
-            .get(AddFriendsViewModel::class.java)
+        mViewmodel = initViewModel()
 
         back_image.setOnClickListener { finish() }
         add_friends_recycler.adapter = mAdapter
@@ -56,7 +56,6 @@ class AddFriendsActivity : AppCompatActivity(),
     private fun setFollow(uid: String, follow: Boolean, onSuccess: () -> Unit) {
         mViewmodel.setFollow(mUser.uid, uid, follow)
             .addOnSuccessListener { onSuccess() }
-            .addOnFailureListener { showToast(it.message) }
     }
 
     companion object {
